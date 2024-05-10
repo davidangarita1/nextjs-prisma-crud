47cc47164cf0fd225d5eb069c6c8281395b3be08
david-angarita <davidangarita.sistemas@gmail.com>
2024-05-09 20:11:48 -0500

    se crea crud

---
+++ src/app/api/tasks/[id]/route.ts
@@ -0,0 +1,36 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/libs/prisma";
+
+interface GetProps {
+  params: {
+    id: number;
+  };
+}
+
+export async function GET(request: Request, { params }: GetProps) {
+  const result = await prisma.task.findUnique({
+    where: { id: Number(params.id) },
+  });
+  return NextResponse.json({ status: "success", data: result });
+}
+
+export async function PUT(request: Request, { params }: GetProps) {
+  const data = await request.json();
+  const taskUpdated = await prisma.task.update({
+    where: { id: Number(params.id) },
+    data,
+  });
+
+  return NextResponse.json({ status: "success", data: taskUpdated });
+}
+
+export async function DELETE(request: Request, { params }: GetProps) {
+  try {
+    const [taskRemoved]: any = await prisma.task.delete({
+      where: { id: Number(params.id) },
+    });
+    return NextResponse.json({ status: "success", data: taskRemoved.id });
+  } catch (error) {
+    return NextResponse.json({ status: "failed", data: error });
+  }
+}
--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -1,5 +1,18 @@
 import { NextResponse } from "next/server";
+import { prisma } from "@/libs/prisma";
 
-export function GET() {
-    return NextResponse.json("creando tareas")
+export async function GET() {
+  const tasks = await prisma.task.findMany();
+  return NextResponse.json({ status: "success", data: tasks });
+}
+
+export async function POST(request: Request) {
+  const { title, description } = await request.json();
+  const newTask = await prisma.task.create({
+    data: {
+      title,
+      description,
+    },
+  });
+  return NextResponse.json({ status: "success", data: newTask });
 }
