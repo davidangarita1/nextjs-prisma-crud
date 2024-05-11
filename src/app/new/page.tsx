86f8c70bd85346b9a93187c31562c9f81fa8d113
david-angarita <davidangarita.sistemas@gmail.com>
2024-05-10 20:17:21 -0500

    se crea post

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,8 +1,50 @@
+"use client";
+import { useRouter } from "next/navigation";
 
 export default function NewPage() {
+  const router = useRouter();
+  const onSubmit = async (e: any) => {
+    e.preventDefault();
+    const title = e.target.title.value;
+    const description = e.target.description.value;
+
+    const res = await fetch("/api/tasks", {
+      method: "POST",
+      body: JSON.stringify({ title, description }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+    console.log(data);
+    router.push("/");
+  };
+
   return (
-    <div>
-      <h1>Formulario de tareas</h1>
+    <div className="h-screen flex justify-center items-center">
+      <form className="bg-slate-800 p-10 lg:w-1/4 md:w-1/2" onSubmit={onSubmit}>
+        <label htmlFor="title" className="font-bold text-sm">
+          Título de la tarea
+        </label>
+        <input
+          type="text"
+          id="title"
+          className="border border-gray-400 p-2 mb-4 w-full text-black"
+          placeholder="Título"
+        />
+        <label htmlFor="description" className="font-bold text-sm">
+          Descripción de la tarea
+        </label>
+        <textarea
+          rows={3}
+          id="description"
+          className="border border-gray-400 p-2 mb-4 w-full text-black"
+          placeholder="Describe tu tarea"
+        ></textarea>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Crear
+        </button>
+      </form>
     </div>
   );
 }
